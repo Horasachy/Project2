a1db9ef4abff88dcfb4fa94843f9e10e7f5cd4d9
Horasachy <49554463+Horasachy@users.noreply.github.com>
2019-05-21 16:12:04 +0500

    active-link.js

--- views/layouts/admin_header.php
+++ views/layouts/admin_header.php
@@ -43,7 +43,7 @@
 			<div class="row">
 				<div class="col-md-9">
 					<div class="logo">
-						<img class="logo_image" src="../../public/images/logo_green.png">
+						<a href="/"><img class="logo_image" src="../../public/images/logo_green.png"></a>
 					</div>
 				</div>
 				<div class="col-md-3">
@@ -58,40 +58,24 @@
 			</div>
 		</div>
 		<!-- Меню -->
-		<nav class="navbar navbar-expand-lg navbar-light" style="background-color: white; margin-top: 10px;
-		font-weight: bold;
-		-webkit-box-shadow: 0px 5px 5px 1px rgba(0,0,0,0.23);
-		-moz-box-shadow: 0px 5px 5px 1px rgba(0,0,0,0.23);
-		box-shadow: 0px 5px 5px 1px rgba(0,0,0,0.23);">
-		<a class="navbar-brand" href="#">BASK</a>
+		<nav class="navbar navbar-expand-lg navbar-light">
+		<a class="navbar-brand" href="/">BASK</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbar1">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
-					<a class="nav-link" href="#">Главная<span class="sr-only">(current)</span></a>
+					<a class="nav-link" href="/adminNews">Главная<span class="sr-only">(current)</span></a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#">О нас</a>
 				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Курсы</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-						<a class="dropdown-item" href="#">Action</a>
-						<a class="dropdown-item" href="#">Another action</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="#">Something else here</a>
-					</div>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Курсы</a>
 				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Электронное образование</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-						<a class="dropdown-item" href="#">Action</a>
-						<a class="dropdown-item" href="#">Another action</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="#">Something else here</a>
-					</div>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Электронное образование</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#">Детская школа 3D-SIZE</a>
--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -2,6 +2,7 @@
 				Информация
 			</div>
 	<script src="../../public/js/jquery.min.js"></script>
+	<script src="../../public/js/active-link.js"></script>
 	<script src="../../public/js/all.js"></script>
 	<script src="../../public/js/bootstrap.min.js"></script>
 	<script src="../../public/js/formFeedBackaJax.js"></script>
